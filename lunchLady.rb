98ae6d83c8898fd8dea95ccd9f8bce325f290d2c
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-08 13:40:26 -0600

    started the lunchlady app

---
+++ lunchLady.rb
@@ -0,0 +1,27 @@
+require "pry"
+
+class MainDish
+  attr_accessor :name, :price
+
+
+    
+end 
+  
+class SideDish
+  attr_accessor :name, :price
+end 
+  
+  
+class LunchLady
+    
+    def initialize
+      @main_dishes = []
+      # user name
+    # money
+  end
+
+end
+
+LunchLady.new
+
+#think about how you would store the information
